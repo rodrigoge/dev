fef1448cad4e3f1c085ef2469c732d50a797b607
rodrigoge <rgestevao@gmail.com>
2020-07-08 17:45:55 -0300

    Updates

---
+++ precache-manifest.d41d1835c64980902cccb367266b1d3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2744c7aab55a157be63997ad8e5f33e",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "4de42d0cba77adeec7a8",
+    "url": "/dev/static/css/main.c16364d4.chunk.css"
+  },
+  {
+    "revision": "34c61832cab86a6f1858",
+    "url": "/dev/static/js/2.a5727bc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dev/static/js/2.a5727bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de42d0cba77adeec7a8",
+    "url": "/dev/static/js/main.21a59fe6.chunk.js"
+  },
+  {
+    "revision": "641080422fbdb781daff",
+    "url": "/dev/static/js/runtime-main.5ab8c36a.js"
+  },
+  {
+    "revision": "4884175a616e4a83fc013a470e901d34",
+    "url": "/dev/static/media/profile.4884175a.jpg"
+  }
+]);
